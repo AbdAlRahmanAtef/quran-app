f46397825cb1bb7947767f371e5fce56f7c38401
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2022-12-21 08:14:19 +0200

    modified

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-const nextConfig = {
-  typeScript: {
-    ignoreBuildErrors: true,
-  },
+const withPWA = require("next-pwa")({
+  dest: "public",
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  scope: "/app",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
-module.exports = nextConfig;
+});
