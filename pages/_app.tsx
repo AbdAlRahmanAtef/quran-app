8fb4fe51bf2a6c0677f0b7b320a344fc7166d4e4
Abdalrhman Atef <abdalrhman.atef.ramadan@gmail.com>
2022-12-21 09:22:31 +0200

    modified

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import Layout from "../components/Layout";
 import "../styles/main.scss";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
-import Head from 'next/head';
+import Head from "next/head";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <NextNProgress
+        options={{ easing: "ease", speed: 500 }}
+        showOnShallow={false}
+      />
       <Head>
         <title>القران الكريم</title>
       </Head>
